e8df7360e542672f244aa9b08f20165b1d82ae5b
ericong18 <ericong18@gmail.com>
2019-09-06 22:57:18 -0400

    Use upload button to upload multiple files to endpoint

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { Modal, Button } from 'antd';
 import './Home.css';
 
+const BASE_URL = "http://5938164a.ngrok.io";
+
 class Home extends Component {
+  inputRef = React.createRef();
+
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    isUploading: false
   }
 
   handleClick = () => {
@@ -15,9 +20,40 @@ class Home extends Component {
     this.setState({ modalIsOpen: false });
   }
 
+  handleChange = e => {
+    e.preventDefault();
+
+    this.setState({ isUploading: true });
+
+    const formData = new FormData();
+    const files = e.target.files;
+
+    Array.from(files).forEach(file => {
+      formData.append('files', file)
+    })
+
+    fetch(`${BASE_URL}/submitTour`, {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ isUploading: true });
+      })
+      .catch(error => {
+        console.error(error);
+        this.setState({ isUploading: true });
+      })
+  }
+
+  handleUploadButtonClick = () => {
+    this.inputRef.current.click();
+  }
+
   render() {
     return (
-      <div className="home-container">
+      <div className="home">
         <Button type="primary" onClick={this.handleClick}>Create</Button>
         <Modal
           centered
@@ -25,9 +61,12 @@ class Home extends Component {
           onCancel={this.handleCancel}
           footer={[]}
         >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+          <div className="container-fluid">
+            <input type="file" name="files" ref={this.inputRef} onChange={this.handleChange} multiple hidden/>
+            <div className="row justify-content-center">
+              <Button type="primary" onClick={this.handleUploadButtonClick} ghost>Upload Files</Button>
+            </div>
+          </div>
         </Modal>
       </div>
     )
