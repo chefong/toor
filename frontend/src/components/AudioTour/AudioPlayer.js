1d43e54cdf19bc752b4bed30cc05ef2db220b873
Pyi Them Kyaw <pyithemkyaw@huntsman-ve702-3546.apng.wlan.private.upenn.edu>
2019-09-07 15:27:03 -0400

    done I think

--- frontend/src/components/AudioTour/AudioPlayer.js
+++ frontend/src/components/AudioTour/AudioPlayer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
 import './AudioPlayer.css'
-
+//http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3
 class AudioPlayer extends Component {
   render () {
     return <ReactPlayer 
      id="audio-player"
      controls={true}
-     url='http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3' 
+     url='https://www.computerhope.com/jargon/m/example.mp3' 
      fileConfig={{ forceAudio: true }}
      width="90%"
      height="50px"
