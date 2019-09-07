4b918ed9c1fe962987f9c8f47686fe80f8e629fb
ericong18 <ericong18@gmail.com>
2019-09-06 20:56:36 -0400

    Set up modal

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Modal, Button } from 'antd';
 import './Home.css';
 
 class Home extends Component {
+  state = {
+    modalIsOpen: false
+  }
+
+  handleClick = () => {
+    this.setState({ modalIsOpen: true });
+  }
+
+  handleCancel = () => {
+    this.setState({ modalIsOpen: false });
+  }
+
   render() {
     return (
       <div className="home-container">
-        Hi I'm Home
+        <Button type="primary" onClick={this.handleClick}>Create</Button>
+        <Modal
+          centered
+          visible={this.state.modalIsOpen}
+          onCancel={this.handleCancel}
+          footer={[]}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
       </div>
     )
   }
